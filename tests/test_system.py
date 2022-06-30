7ef5ffb39179cf36ed0b9bc00e3c37f43e78eefb
hexatester <hexatester@protonmail.com>
2022-07-01 00:03:37 +0700

    Improve TestSystem

--- tests/test_system.py
+++ tests/test_system.py
@@ -1,6 +1,31 @@
 from ros import System
+from ros.system import Health, History, Identity, License, Note, Resource, RouterBOARD
+from typing import List
 
 
 class TestSystem:
     def test_system(self, ros):
         assert isinstance(ros.system, System)
+
+    def test_health(self, ros):
+        for health in ros.system.health:
+            assert isinstance(health, Health)
+
+    def test_history(self, ros):
+        for history in ros.system.history:
+            assert isinstance(history, History)
+
+    def test_identity(self, ros):
+        assert isinstance(ros.system.identity, Identity)
+
+    def test_license(self, ros):
+        assert isinstance(ros.system.license, License)
+
+    def test_note(self, ros):
+        assert isinstance(ros.system.note, Note)
+
+    def test_resource(self, ros):
+        assert isinstance(ros.system.resource, Resource)
+
+    def test_routerboard(self, ros):
+        assert isinstance(ros.system.routerboard, RouterBOARD)
