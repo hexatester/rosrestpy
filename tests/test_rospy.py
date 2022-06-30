8175782f6d3c4481f2abef6d2379c224cce1f5f7
hexatester <hexatester@protonmail.com>
2022-07-01 00:07:22 +0700

    Added TestRos.test_log

--- tests/test_rospy.py
+++ tests/test_rospy.py
@@ -1,6 +1,10 @@
-from ros import __version__
+from ros import __version__, Log
 
 
 class TestRos:
     def test_version(self):
         assert __version__ == "0.1.1"
+
+    def test_log(self, ros):
+        for log in ros.log:
+            assert isinstance(log, Log)
