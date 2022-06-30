3eb000d78cc2e3721e16594037f8fc459191173b
hexatester <hexatester@protonmail.com>
2022-06-30 23:36:09 +0700

    Initial conftest

---
+++ tests/conftest.py
@@ -0,0 +1,27 @@
+import os
+from pytest import fixture
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from ros import Ros
+
+
+@fixture
+def server():
+    return os.getenv("server")
+
+
+@fixture
+def username():
+    return os.getenv("username")
+
+
+@fixture
+def password():
+    return os.getenv("password")
+
+
+@fixture
+def ros(server: str, username: str, password: str):
+    return Ros(server, username, password)
