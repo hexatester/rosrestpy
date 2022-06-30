1826e46350f6b90706550081a83cee9bc260d526
hexatester <hexatester@protonmail.com>
2022-06-30 23:15:33 +0700

    Rename project

--- ros/system/__init__.py
+++ ros/system/__init__.py
@@ -2,7 +2,7 @@ from attr import define
 from typing import TYPE_CHECKING, List
 
 if TYPE_CHECKING:
-    from rospy.rospy import Rospy
+    from ros.ros import Ros
 
 from .health import Health
 from .history import History
@@ -15,7 +15,7 @@ from .routerboard import RouterBOARD
 
 @define
 class System:
-    rospy: "Rospy"
+    ros: "Ros"
     url: str = ""
 
     def __attrs_post_init__(self) -> None:
@@ -23,31 +23,31 @@ class System:
 
     @property
     def health(self) -> List[Health]:
-        return self.rospy.get_as(self.url + "/health", List[Health])
+        return self.ros.get_as(self.url + "/health", List[Health])
 
     @property
     def history(self) -> List[History]:
-        return self.rospy.get_as(self.url + "/history", List[History])
+        return self.ros.get_as(self.url + "/history", List[History])
 
     @property
     def identity(self) -> Identity:
-        return self.rospy.get_as(self.url + "/identity", Identity)
+        return self.ros.get_as(self.url + "/identity", Identity)
 
     @property
     def license(self) -> License:
-        return self.rospy.get_as(self.url + "/license", License)
+        return self.ros.get_as(self.url + "/license", License)
 
     @property
     def note(self) -> Note:
-        return self.rospy.get_as(self.url + "/note", Note)
+        return self.ros.get_as(self.url + "/note", Note)
 
     @property
     def resource(self) -> Resource:
-        return self.rospy.get_as(self.url + "/resource", Resource)
+        return self.ros.get_as(self.url + "/resource", Resource)
 
     @property
     def routerboard(self) -> RouterBOARD:
-        return self.rospy.get_as(self.url + "/routerboard", RouterBOARD)
+        return self.ros.get_as(self.url + "/routerboard", RouterBOARD)
 
 
 __all__ = [
