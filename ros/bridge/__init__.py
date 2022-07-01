8f57cce6b2c48a45bdd0cf0f5ab6b3f95dff8e6d
hexatester <hexatester@protonmail.com>
2022-07-01 20:10:05 +0700

    Added BridgeInterface

--- ros/bridge/__init__.py
+++ ros/bridge/__init__.py
@@ -1,11 +1,15 @@
+from typing import List
 from attrs import define
 
 from ros.base import BaseModule
 
+from .interface import BridgeInterface
+
 
 @define
 class Bridge(BaseModule):
-    pass
+    def print(self) -> List[BridgeInterface]:
+        return self.ros.get_as(self.url, List[BridgeInterface])
 
 
-__all__ = ["Bridge"]
+__all__ = ["Bridge", "BridgeInterface"]
---
+++ ros/bridge/interface.py
@@ -0,0 +1,26 @@
+from attr import dataclass
+
+
+@dataclass
+class BridgeInterface:
+    id: str
+    actual_mtu: int
+    ageing_time: str
+    arp: str
+    arp_timeout: str
+    auto_mac: bool
+    dhcp_snooping: bool
+    disabled: bool
+    fast_forward: bool
+    forward_delay: str
+    igmp_snooping: bool
+    l2mtu: int
+    mac_address: str
+    max_message_age: str
+    mtu: str
+    name: str
+    priority: str
+    protocol_mode: str
+    running: bool
+    transmit_hold_count: int
+    vlan_filtering: bool
