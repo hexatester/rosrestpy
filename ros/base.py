ff57d6b0006d741270e38ea8fdd0e3a3be2dba68
hexatester <hexatester@protonmail.com>
2022-07-01 08:43:31 +0700

    Added BaseSubModule

--- ros/base.py
+++ ros/base.py
@@ -1,5 +1,5 @@
 from attr import define
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Type, TypeVar
 
 if TYPE_CHECKING:
     from ros.ros import Ros
@@ -14,4 +14,19 @@ class BaseModule:
         if self.url:
             self.url = "/" + self.url
         else:
-        self.url = "/" + self.__class__.__name__.lower()
+            self.url = "/" + self.__class__.__name__.lower()
+
+
+BM = TypeVar("BM", bound=BaseModule)
+
+
+@define
+class BaseSubModule:
+    module: BM
+    url: str = ""
+
+    def __attrs_post_init__(self) -> None:
+        if self.url:
+            self.url = self.module.url + "/" + self.url
+        else:
+            self.url = self.module.url + "/" + self.__class__.__name__.lower()
