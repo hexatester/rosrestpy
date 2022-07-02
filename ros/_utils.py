540c9c97dfec9498aeae52638ce64bc854c3c8cd
hexatester <hexatester@protonmail.com>
2022-07-02 19:05:07 +0700

    Update clean_key

--- ros/_utils.py
+++ ros/_utils.py
@@ -6,8 +6,7 @@ def clean_key(d: Dict[str, Any]) -> dict:
     nd = dict()
     for k, v in d.items():
         k = k.replace("-", "_")
-        if k == ".id":
-            k = "id"
+        k = k.replace(".", "")
         nd[k] = v
     return nd
 
