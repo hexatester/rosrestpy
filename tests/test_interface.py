e62f2cf6cb1259e431f745fe1560b78be7ac5978
hexatester <hexatester@protonmail.com>
2022-07-04 12:28:38 +0700

    Initial TestBridge

--- tests/test_interface.py
+++ tests/test_interface.py
@@ -1,6 +1,15 @@
 from ros import InterfaceModule
+from ros.inteface import BridgeModule, BridgePort
 
 
 class TestInterface:
     def test_interface(self, ros):
         assert isinstance(ros.interface, InterfaceModule)
+
+
+class TestBridge:
+    def test_bridge(self, ros):
+        assert isinstance(ros.bridge, BridgeModule)
+
+    def test_bridge_port(self, ros):
+        assert isinstance(ros.bridge.port, BridgePort)
