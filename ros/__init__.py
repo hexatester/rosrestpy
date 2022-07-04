38e1d66b934045d0aa705abfc9ee5a9e48bb069e
hexatester <hexatester@protonmail.com>
2022-07-04 14:32:12 +0700

    Move _converter to base

--- ros/__init__.py
+++ ros/__init__.py
@@ -1,4 +1,7 @@
 __version__ = "0.1.5"
+from ._utils import make_converter
+
+_converter = make_converter()
 
 from .inteface import InterfaceModule
 from .ip import IPModule
@@ -7,6 +10,7 @@ from .system import SystemModule
 from .error import Error
 from .log import Log
 from .ping import Ping
+
 
 from .ros import Ros
 
