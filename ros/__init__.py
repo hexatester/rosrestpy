0821ec19149264d46b553e05e43687c229aad11e
hexatester <hexatester@protonmail.com>
2022-07-04 13:58:32 +0700

    Bumping version from 0.1.4 to 0.1.5

--- ros/__init__.py
+++ ros/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 
 from .inteface import InterfaceModule
 from .ip import IPModule
--- tests/test_rospy.py
+++ tests/test_rospy.py
@@ -3,7 +3,7 @@ from ros import __version__, Log
 
 class TestRos:
     def test_version(self):
-        assert __version__ == "0.1.4"
+        assert __version__ == "0.1.5"
 
     def test_log(self, ros):
         for log in ros.log:
