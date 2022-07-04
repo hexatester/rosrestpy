585e8bb6a5130e15284d602587d698fab951e747
hexatester <hexatester@protonmail.com>
2022-07-04 14:47:13 +0700

    Refactor ToolModule using Ros.post_as

--- ros/tool/__init__.py
+++ ros/tool/__init__.py
@@ -1,10 +1,6 @@
-import json
 from typing import List
 
-from ros import _converter
-from ros.error import Error
 from ros._base import BaseModule
-from ros._utils import clean_data
 
 from .ping import Ping
 
@@ -12,11 +8,4 @@ from .ping import Ping
 class ToolModule(BaseModule):
     def ping(self, address: str, count: int = 4):
         data = {"address": address, "count": count}
-        res = self.ros.session.post(
-            self.ros.url + self.url + "/ping", json=data, verify=self.ros.secure
-        )
-        odata = json.loads(res.text)
-        data = clean_data(odata)
-        if data and "error" in data:
-            raise _converter.structure(data, Error)
-        return _converter.structure(data, List[Ping])
+        return self.ros.post_as("/ping", List[Ping], data)
