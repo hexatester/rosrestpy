b2e34e444e99bc9ab202d03f33121ec1d2173a05
hexatester <hexatester@protonmail.com>
2022-07-04 15:16:41 +0700

    Update BandwithTest types

--- ros/tool/bandwith_test.py
+++ ros/tool/bandwith_test.py
@@ -9,11 +9,16 @@ class BandwithTest:
     direction: str
     lost_packets: int
     random_data: bool
-    rx_10_second_average: int
-    rx_current: int
-    rx_size: int
-    rx_total_average: int
     status: str
+    rx_10_second_average: Optional[int] = None
+    rx_current: Optional[int] = None
+    rx_size: Optional[int] = None
+    rx_total_average: Optional[int] = None
+    tx_10_second_average: Optional[int] = None
+    tx_current: Optional[int] = None
+    tx_size: Optional[int] = None
+    tx_total_average: Optional[int] = None
+    about: Optional[str] = None
     duration: Optional[str] = None
     local_cpu_load: Optional[int] = None
     remote_cpu_load: Optional[int] = None
