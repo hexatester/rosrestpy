2c6272b98cbd87dd153c4020a2c90fe843d523fa
hexatester <hexatester@protonmail.com>
2022-09-16 20:12:34 +0700

    Black

--- ros/ip/dns/_literals.py
+++ ros/ip/dns/_literals.py
@@ -1,3 +1,5 @@
 from typing import Literal
 
-DNSType = Literal["A", "AAAA", "CNAME", "FWD", "MX", "NS", "NXDOMAIN", "PTR", "SRV", "TXT"]
+DNSType = Literal[
+    "A", "AAAA", "CNAME", "FWD", "MX", "NS", "NXDOMAIN", "PTR", "SRV", "TXT"
+]
