041735afb2a71995c8afae5fde3216da7d3ef180
hexatester <hexatester@protonmail.com>
2022-12-11 13:31:35 +0700

    Added /queue/simple setter

--- ros/queue/__init__.py
+++ ros/queue/__init__.py
@@ -2,6 +2,7 @@ from attrs import define
 from typing import List
 
 from ros._base import BaseModule
+from ros._utils import make_setters
 from .interface import InterfaceQueue
 from .simple import SimpleQueue
 
@@ -15,3 +16,5 @@ class QueueModule(BaseModule):
     @property
     def simple(self) -> List[SimpleQueue]:
         return self.ros.get_as(self.url + "/simple", List[SimpleQueue])
+
+    simple.setter = make_setters("/queue/simple", SimpleQueue)
