ec7056e209baa8e69d32b488a1be4a80c68d9b06
hexatester <hexatester@protonmail.com>
2022-12-16 06:48:03 +0700

    Update TestIP

--- tests/test_ip.py
+++ tests/test_ip.py
@@ -9,22 +9,22 @@ class TestIP:
         assert isinstance(ros.ip, IPModule)
 
     def test_address(self, ros):
-        for address in ros.ip.address:
+        for address in ros.ip.address():
             assert isinstance(address, Address)
 
     def test_arp(self, ros):
-        for arp in ros.ip.arp:
+        for arp in ros.ip.arp():
             assert isinstance(arp, ARP)
 
     def test_cloud(self, ros):
         assert isinstance(ros.ip.cloud, Cloud)
 
     def test_dhcp_client(self, ros):
-        for client in ros.ip.dhcp_client:
+        for client in ros.ip.dhcp_client():
             assert isinstance(client, DHCPClient)
 
     def test_dhcp_relay(self, ros):
-        for relay in ros.ip.dhcp_relay:
+        for relay in ros.ip.dhcp_relay():
             assert isinstance(relay, DHCPRelay)
 
 
@@ -33,7 +33,7 @@ class TestDHCPServer:
         assert isinstance(ros.ip.dhcp_server, DHCPServerModule)
 
     def test_item(self, ros):
-        for item in ros.ip.dhcp_server.print():
+        for item in ros.ip.dhcp_server():
             assert isinstance(item, DHCPServer)
 
     def test_network(self, ros):
@@ -46,12 +46,12 @@ class TestDNS:
         assert isinstance(ros.ip.dns, DNS)
 
     def test_cache(self, ros):
-        for i in ros.ip.dns.cache:
+        for i in ros.ip.dns.cache():
             assert isinstance(i, DNSCache)
 
     def test_cache_flush(self, ros):
         assert ros.ip.dns.flush() is None
 
     def test_static(self, ros):
-        for i in ros.ip.dns.static:
+        for i in ros.ip.dns.static():
             assert isinstance(i, DNSStatic)
