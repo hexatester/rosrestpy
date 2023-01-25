37d808c84d62df413127aca6f13358aabd6db1be
hexatester <hexatester@protonmail.com>
2023-01-25 11:51:04 +0700

    Initial /ip/firewall/connection/pr

--- ros/ip/firewall/connection/__init__.py
+++ ros/ip/firewall/connection/__init__.py
@@ -1,5 +1,12 @@
+from typing import Any, List
+
 from ros._base import BaseSubModule
+from .connection import IPConnection
 
 
 class IPFirewallConnectionModule(BaseSubModule):
-    pass
+    def __call__(self, **kwds: Any) -> List[IPConnection]:
+        return self.print(**kwds)
+
+    def print(self, **kwds: Any) -> List[IPConnection]:
+        return self.module.ros.get_as(self.url, List[IPConnection], kwds)
---
+++ ros/ip/firewall/connection/connection.py
@@ -0,0 +1,36 @@
+from attr import dataclass
+from typing import Optional
+
+from ros._literals import IPProtocol, TCPState
+
+
+@dataclass(slots=True)
+class IPConnection:
+    id: str
+    assured: bool
+    confirmed: bool
+    dst_address: str
+    dstnat: bool
+    dying: bool
+    expected: bool
+    fasttrack: bool
+    hw_offload: bool
+    orig_bytes: int
+    orig_fasttrack_bytes: int
+    orig_fasttrack_packets: int
+    orig_packets: int
+    orig_rate: int
+    protocol: IPProtocol
+    repl_bytes: int
+    repl_fasttrack_bytes: int
+    repl_fasttrack_packets: int
+    repl_packets: int
+    repl_rate: int
+    reply_dst_address: str
+    reply_src_address: str
+    seen_reply: bool
+    src_address: str
+    srcnat: bool
+    timeout: str
+    connection_mark: Optional[str] = None
+    tcp_state: Optional[TCPState] = None
