dd327c748308c0566ee46044d1d8201ff9b42721
hexatester <hexatester@protonmail.com>
2023-02-23 22:26:41 +0700

    Improve /ip/route types

--- ros/ip/route.py
+++ ros/ip/route.py
@@ -1,24 +1,27 @@
-from typing import Optional
-from attr import dataclass
+from attr import dataclass, field
+from typing import Literal, Optional
 
 
 @dataclass
 class Route:
-    id: str
-    distance: int
     dst_address: str
-    dynamic: bool
-    ecmp: bool
     gateway: str
-    hw_offloaded: bool
-    immediate_gw: str
-    inactive: bool
     routing_table: str
-    scope: int
-    suppress_hw_offload: bool
-    active: bool = False
-    pref_src: Optional[str] = None
+    check_gateway: Literal["arp", "bfd", "bfd-multihop", "none", "ping"] = None
+    suppress_hw_offload: bool = False
+    distance: int = 1
+    scope: int = 30
     target_scope: Optional[int] = None
-    vpn: Optional[bool] = None
     vrf_interface: Optional[str] = None
+    pref_src: Optional[str] = None
+    vpn: Optional[bool] = None
     comment: Optional[str] = None
+    immediate_gw: str = field(on_setattr=None, default=None)
+    active: bool = field(on_setattr=None, default=None)
+    dynamic: bool = field(on_setattr=None, default=None)
+    ecmp: bool = field(on_setattr=None, default=None)
+    hw_offloaded: bool = field(on_setattr=None, default=None)
+    inactive: bool = field(on_setattr=None, default=None)
+    static: bool = field(on_setattr=None, default=None)
+    filtered: bool = field(on_setattr=None, default=None)
+    id: str = field(on_setattr=None, default=None)
