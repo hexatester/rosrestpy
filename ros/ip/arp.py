50946e6d4bfa846402fbe14b364db63d01e53c66
hexatester <hexatester@protonmail.com>
2023-02-23 22:26:16 +0700

    Improve /ip/arp types

--- ros/ip/arp.py
+++ ros/ip/arp.py
@@ -1,17 +1,17 @@
-from attr import dataclass
+from attr import dataclass, field
 from typing import Optional
 
 
 @dataclass
 class ARP:
-    id: str
-    DHCP: bool
     address: str
-    complete: bool
-    disabled: bool
-    dynamic: bool
     interface: str
-    invalid: bool
-    published: bool
     mac_address: Optional[str] = None
+    published: bool = False
+    disabled: bool = False
     comment: Optional[str] = None
+    id: str = field(on_setattr=None, default=None)
+    DHCP: Optional[bool] = field(on_setattr=None, default=None)
+    complete: Optional[bool] = field(on_setattr=None, default=None)
+    dynamic: Optional[bool] = field(on_setattr=None, default=None)
+    invalid: Optional[bool] = field(on_setattr=None, default=None)
