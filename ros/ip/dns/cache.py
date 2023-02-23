8ef9c172d7f4d63dbdfc9ea48a4c38f975449938
hexatester <hexatester@protonmail.com>
2023-02-23 19:54:32 +0700

    Update DNSCache.data type

--- ros/ip/dns/cache.py
+++ ros/ip/dns/cache.py
@@ -1,4 +1,5 @@
 from attr import dataclass
+from typing import Optional
 
 from ._literals import DNSType
 
@@ -6,8 +7,8 @@ from ._literals import DNSType
 @dataclass(slots=True)
 class DNSCache:
     id: str
-    data: str
     name: str
     static: bool
     ttl: str
     type: DNSType
+    data: Optional[str] = None
