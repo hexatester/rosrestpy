44a5c0ff12f08a4a98545cd2f6c98bf06a0ae5d8
hexatester <hexatester@protonmail.com>
2023-02-23 23:23:00 +0700

    Improve /queue/simple types

--- ros/queue/simple.py
+++ ros/queue/simple.py
@@ -1,42 +1,58 @@
-from attr import dataclass
+from attr import dataclass, field
 from typing import Optional
 
 
 @dataclass
 class SimpleQueue:
-    id: str
-    bucket_size: str
-    burst_limit: str
-    burst_threshold: str
-    burst_time: str
-    bytes: str
-    disabled: bool
-    dropped: str
-    dynamic: bool
-    invalid: bool
-    limit_at: str
-    max_limit: str
+    # General
     name: str
-    packet_marks: str
-    packet_rate: str
-    packets: str
-    parent: Optional[str]
-    priority: str
-    queue: str
-    queued_bytes: str
-    queued_packets: str
-    rate: str
     target: str
-    total_bytes: int
-    total_dropped: int
-    total_packet_rate: int
-    total_packets: int
-    total_queued_bytes: int
-    total_queued_packets: int
-    total_rate: int
-    comment: Optional[str] = None
+    max_limit: str = "0/0"
     dst: Optional[str] = None
+    burst_limit: str = None
+    burst_threshold: str = None
+    burst_time: str = None
+    time: str = None
+    disabled: bool = False
+    comment: Optional[str] = None
+    # Advanced
+    packet_marks: Optional[str] = None
+    limit_at: str = "0/0"
+    priority: str = "8/8"
+    bucket_size: str = "0.100/0.100"
+    queue: Optional[str] = None
+    parent: Optional[str] = None
+    # Total
+    total_bucket_size: Optional[str] = None
+    total_burst_threshold: Optional[str] = None
+    total_limit_at: Optional[str] = None
+    total_priority: Optional[int] = None
+    total_burst_limit: Optional[str] = None
+    total_burst_time: Optional[str] = None
+    total_max_limit: Optional[str] = None
     total_queue: Optional[str] = None
+    # Statistics
+    rate: str = field(on_setattr=None, default=None)
+    packet_rate: str = field(on_setattr=None, default=None)
+    queued_bytes: str = field(on_setattr=None, default=None)
+    queued_packets: str = field(on_setattr=None, default=None)
+    bytes: str = field(on_setattr=None, default=None)
+    packets: str = field(on_setattr=None, default=None)
+    dropped: str = field(on_setattr=None, default=None)
+    pcq_queues: Optional[str] = field(on_setattr=None, default=None)
+    # Total Statistics
+    total_rate: str = field(on_setattr=None, default=None)
+    total_packet_rate: int = field(on_setattr=None, default=None)
+    total_queued_bytes: str = field(on_setattr=None, default=None)
+    total_queued_packets: int = field(on_setattr=None, default=None)
+    total_bytes: str = field(on_setattr=None, default=None)
+    total_packets: int = field(on_setattr=None, default=None)
+    total_dropped: int = field(on_setattr=None, default=None)
+    total_pcq_queues: Optional[str] = field(on_setattr=None, default=None)
+    # ETC
+    id: str = field(on_setattr=None, default=None)
+    dynamic: bool = field(on_setattr=None, default=None)
+    invalid: bool = field(on_setattr=None, default=None)
 
     def __str__(self) -> str:
         return self.name
