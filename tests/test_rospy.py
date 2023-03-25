7bf3092f40519f5ee38ac95247ed51f94c310143
hexatester <hexatester@protonmail.com>
2023-03-25 08:47:16 +0700

    Bumping version from 0.5.1 to 0.5.2

--- tests/test_rospy.py
+++ tests/test_rospy.py
@@ -3,7 +3,7 @@ from ros import __version__, Log, Ros
 
 class TestRos:
     def test_version(self):
-        assert __version__ == "0.5.1"
+        assert __version__ == "0.5.2"
 
     def test_log(self, ros: Ros):
         for log in ros.log():
