a7371c7954cb3394341b31d4998c282e4bfab1a1
hexatester <hexatester@protonmail.com>
2023-07-02 21:35:09 +0700

    refactor tests

--- tests/test_interface.py
+++ tests/test_interface.py
@@ -6,33 +6,33 @@ from ros.inteface.list import InterfaceListModule, InterfaceList, InterfaceListM
 
 
 class TestInterface:
-    def test_interface(self, ros: Ros):
+    def test_module(self, ros: Ros):
         assert isinstance(ros.interface, InterfaceModule)
 
 
 class TestBridge:
-    def test_bridge(self, ros: Ros):
+    def test_module(self, ros: Ros):
         assert isinstance(ros.bridge, BridgeModule)
 
-    def test_bridge_bridge(self, ros: Ros):
+    def test_bridge(self, ros: Ros):
         for item in ros.bridge():
             assert isinstance(item, Bridge)
 
-    def test_bridge_msti(self, ros: Ros):
+    def test_msti(self, ros: Ros):
         for item in ros.bridge.msti():
             assert isinstance(item, BridgeMsti)
 
-    def test_bridge_port(self, ros: Ros):
+    def test_port(self, ros: Ros):
         for item in ros.bridge.port():
             assert isinstance(item, BridgePort)
 
-    def test_bridge_vlan(self, ros: Ros):
+    def test_vlan(self, ros: Ros):
         for item in ros.bridge.vlan():
             assert isinstance(item, BridgeVlan)
 
 
 class TestEoIP:
-    def test_eoip_list(self, ros: Ros):
+    def test_eoip(self, ros: Ros):
         for item in ros.interface.eoip():
             assert isinstance(item, EoIP)
 
@@ -41,19 +41,19 @@ class TestEthernet:
     def test_ethernet(self, ros: Ros):
         assert ros.interface.ethernet.cl == InterfaceEthernet
 
-    def test_ethernet_list(self, ros: Ros):
+    def test_list(self, ros: Ros):
         for item in ros.interface.ethernet():
             assert isinstance(item, InterfaceEthernet)
 
 
 class TestList:
-    def test_list(self, ros: Ros):
+    def test_module(self, ros: Ros):
         assert isinstance(ros.interface.list, InterfaceListModule)
 
-    def test_list_list(self, ros: Ros):
+    def test_list(self, ros: Ros):
         for item in ros.interface.list.print():
             assert isinstance(item, InterfaceList)
 
-    def test_list_member(self, ros: Ros):
+    def test_member(self, ros: Ros):
         for item in ros.interface.list.member():
             assert isinstance(item, InterfaceListMember)
