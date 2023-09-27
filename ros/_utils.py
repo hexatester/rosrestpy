04b7001cf4fdfa50036f23b8a45fc4c6fccd2bac
hexatester <hexatester@protonmail.com>
2023-09-28 06:00:39 +0700

    docs for utils

--- ros/_utils.py
+++ ros/_utils.py
@@ -3,6 +3,14 @@ from typing import Any, Dict, Union
 
 
 def clean_key(d: Dict[str, Any]) -> dict:
+    """Replace - with _ and remove . from key
+
+    Args:
+        d (Dict[str, Any]): Raw dict data from ros
+
+    Returns:
+        dict: Cleaned data ready to be used as python object props value
+    """
     nd = dict()
     for k, v in d.items():
         k = k.replace("-", "_")
@@ -12,6 +20,14 @@ def clean_key(d: Dict[str, Any]) -> dict:
 
 
 def clean_data(d: Dict[str, Any]) -> dict:
+    """Clean raw data from ros recursively
+
+    Args:
+        d (Dict[str, Any]): Data from ros
+
+    Returns:
+        dict: Cleaned data
+    """
     data: Any = None
     if isinstance(d, dict):
         data = clean_key(d)
@@ -26,6 +42,14 @@ def clean_data(d: Dict[str, Any]) -> dict:
 
 
 def clean_filters(d: Dict[str, Any]) -> Dict[str, Any]:
+    """Replace _ with - on every key on data for query in url
+
+    Args:
+        d (Dict[str, Any]): Data to be clean
+
+    Returns:
+        Dict[str, Any]: Cleaned data
+    """
     if not d:
         return d
     translation_table = str.maketrans("_", "-")
@@ -33,6 +57,14 @@ def clean_filters(d: Dict[str, Any]) -> Dict[str, Any]:
 
 
 def clean_before_put(d: Dict[str, Any]) -> dict:
+    """Replace _ with - on every key on data for PUT request
+
+    Args:
+        d (Dict[str, Any]): Data to be clean
+
+    Returns:
+        dict: cleaned data
+    """
     if not d:
         return d
     nd = {k.replace("_", "-"): v for k, v in d.items() if v is not None}
@@ -47,6 +79,11 @@ def _union_str_int(v: str, t: Any) -> Union[str, int]:
 
 
 def make_converter() -> Converter:
+    """Making converter for cattrs
+
+    Returns:
+        Converter: cattrs converter
+    """
     c = Converter()
     # Main
     c.register_structure_hook(Union[int, str], _union_str_int)
