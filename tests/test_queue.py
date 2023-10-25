05610387a02677ab980a4ba5d8d54d13a4981f48
hexatester <hexatester@protonmail.com>
2023-10-25 07:20:28 +0700

    Refactor test for /queue

--- tests/test_queue.py
+++ tests/test_queue.py
@@ -6,18 +6,26 @@ class TestQueue:
     def test_queue(self, ros: Ros):
         assert isinstance(ros.queue, QueueModule)
 
+
+class TestQueueInterface:
     def test_queue_interface(self, ros: Ros):
         for i in ros.queue.interface():
             assert isinstance(i, InterfaceQueue)
 
+
+class TestQueueSimple:
     def test_queue_simple(self, ros: Ros):
         for i in ros.queue.simple():
             assert isinstance(i, SimpleQueue)
 
+
+class TestQueueTree:
     def test_queue_tree(self, ros: Ros):
         for i in ros.queue.tree():
             assert isinstance(i, QueueTree)
 
+
+class TestQueueType:
     def test_queue_type(self, ros: Ros):
         for i in ros.queue.type():
             assert isinstance(i, QueueType)
